cbf0caf38662dd9c4e0722b72c2000cf657ba10a
Alex Staelens <Alex.Staelens@vu.com>
2022-02-10 14:37:55 -0600

    added try/except

--- payCalculator.py
+++ payCalculator.py
@@ -3,16 +3,17 @@ def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
     rate = input("Enter Rate: ")
-    
-    fhrs = float(hrs)
-    frate = float(rate)
-    if fhrs> 40:
-        OT= fhrs - 40.0
-        Pay = fhrs*frate + .5*OT*frate
-    else:
-        Pay = fhrs*frate
-    print(Pay)    
-
+    try:
+        fhrs = float(hrs)
+        frate = float(rate)
+        if fhrs> 40:
+           OT= fhrs - 40.0
+           Pay = fhrs*frate + .5*OT*frate
+        else:
+            Pay = fhrs*frate
+        print("Pay:",Pay)    
+    except:
+        print('Enter a number')
     # end assignment
 
 ## if you want to test locally before you try to sync
